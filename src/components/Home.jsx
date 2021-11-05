d43578cbd92a0a2296bb122d3fb05dde765e6a5d
Alejandro Pietry <pietrytrindade12@gmail.com>
2021-11-05 10:48:45 -0300

    corrigindo home para dar erro de cors

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,48 @@
-import React from "react";
+import React, {useState, useEffect}from "react";
+import Url from "../wwwroot/HttpUtils/Url";
+import { HubConnectionBuilder } from "@microsoft/signalr";
 export const Home = () =>{
 
+    const [ connection, setConnection ] = useState(null);
+
+    useEffect(() => {
+        const newConnection = new HubConnectionBuilder()
+            .withUrl(Url.WebApi() + 'hubs/notify')
+            .withAutomaticReconnect()
+            .build();
+
+        setConnection(newConnection);
+    }, []);
+
+    useEffect(() => {
+        if(connection){
+            connection.start()
+            .then(result => {
+
+            })
+        }
+    },[connection])
+
+    const sendMessage = async () => {
+
+        if (connection.connectionStarted) {
+            try {
+                await connection.send('SendNotify', document.getElementById('mensagemNotificacao').value);
+            }
+            catch(e) {
+                console.log(e);
+            }
+        }
+        else {
+            alert('No connection to server yet.');
+        }
+    }
     return(
         <>
             <h3>Finge que aqui tem uma home linda kkkkkk</h3>
             <button><a href="/login">Fazer Login</a></button>
+            <button onClick={sendMessage}>Enviar mensagem</button>
+            <input type="text" id="mensagemNotificacao"></input>
         </>
     )
 }
